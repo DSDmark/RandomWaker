30aa7e463f3833aa981d4d540c98072f3091718f
Sanskar Sahu <sahup3296@gmail.com>
2022-11-04 14:44:15 +0000

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("cvs");
+const ctx = canvas.getContext("2d");
+const CH = canvas.height = 400;
+const CW = canvas.widht = 400;
+
+canvas.style.background = "black";
--- src/components/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as TodoList } from "./TodoList";
-export { default as StoreData } from "./storeData";
-export { default as todoEdit } from "./editTodo";
-export { default as deleteTodo } from "./deleteTodo";
-export { default as addTodo } from "./addTodo";
